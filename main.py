c25dd20da494aa537984f3aeba41ce6c6500f49b
Lexandriinnn_t <sashulya-starikova2005@mail.ru>
2024-06-23 18:52:24 +0300

    Update main.py

--- main.py
+++ main.py
@@ -552,3 +552,8 @@ async def get_total_score_by_query_id(query_id: int):
     finally:
         if conn:
             conn.close()
+
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
