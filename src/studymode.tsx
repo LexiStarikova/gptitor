7e94a22d2afe0dc6bb04e1001d56f4f9edd7d532
Lexandriinnn_t <sashulya-starikova2005@mail.ru>
2024-06-24 13:16:45 +0300

    Update studymode.tsx

--- src/studymode.tsx
+++ src/studymode.tsx
@@ -1,9 +1,15 @@
 import './studymode.css';
-
-const studymode = () => {
+import ConversationPanel from './conversationPanel.tsx';
+import FeedbackPanel from './feedbackPanel.tsx';
+const StudyMode: React.FC = () => {
     return (
-        <div></div>
+        <div>
+            <div className='chatpage'>
+                <ConversationPanel></ConversationPanel>
+                <FeedbackPanel></FeedbackPanel>
+            </div>
+        </div>
     );
-  };
-  
-  export default studymode;
+};
+
+export default StudyMode;
