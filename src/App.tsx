017e04d7562c0992f7fb484536fdef835fb3ef1d
Lexandriinnn_t <sashulya-starikova2005@mail.ru>
2024-06-24 13:03:07 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import SideBar from './sidebar.tsx';
 import NavBar from './header.tsx';
 import {ConversationPanel} from './conversationPanel.tsx';
 import FeedbackPanel from './feedbackPanel.tsx';
+import StudyMode from './studymode.tsx';
+import Profile from './profile.tsx';
 
 function App() {
   return (
     <div>
+      <Router basename="/GPTitor">
       <NavBar></NavBar>
       <div>
         <SideBar></SideBar>
@@ -15,7 +19,14 @@ function App() {
         <ConversationPanel></ConversationPanel>
         <FeedbackPanel></FeedbackPanel>
       </div>
-
+      <main>
+        <Routes>
+          <Route path=" / " element={<Navigate to="/chatpage" />} />
+          <Route path='/chatpage' element={<StudyMode />}></Route>
+          <Route path='/profile' element={<Profile />}></Route>
+        </Routes>
+      </main>
+    </Router>
     </div>
   )
 }
