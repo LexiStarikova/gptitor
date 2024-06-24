c3237caa3b88da8085486361f72a58ab07067f0c
Alexandra Starikova-Nasibullina <a.nasibullina@innopolis.university>
2024-06-24 13:27:06 +0300

    Fix Routing

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,6 @@ import './App.css';
 import { BrowserRouter as Router, Route, Routes, Navigate } from 'react-router-dom';
 import SideBar from './sidebar.tsx';
 import NavBar from './header.tsx';
-import {ConversationPanel} from './conversationPanel.tsx';
-import FeedbackPanel from './feedbackPanel.tsx';
 import StudyMode from './studymode.tsx';
 import Profile from './profile.tsx';
 
@@ -14,10 +12,6 @@ function App() {
       <NavBar></NavBar>
       <div>
         <SideBar></SideBar>
-      </div>
-      <div className='chatpage'>
-        <ConversationPanel></ConversationPanel>
-        <FeedbackPanel></FeedbackPanel>
       </div>
       <main>
         <Routes>
