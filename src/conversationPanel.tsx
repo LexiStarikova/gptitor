a89dbcdabcf39abc924eb95badf1ead3639a0256
Alexandra Starikova-Nasibullina <a.nasibullina@innopolis.university>
2024-06-25 07:34:12 +0300

    Change endpoint of Send Query

--- src/conversationPanel.tsx
+++ src/conversationPanel.tsx
@@ -14,7 +14,8 @@ export const ConversationPanel: React.FC = () => {
         
         try {
             //Link to intermediate (local) API: should be changes frequently!
-            const response = await fetch('https://8662-188-130-155-169.ngrok-free.app/proxy', {
+            //conversation_id = 1; To be modified
+            const response = await fetch('https://8662-188-130-155-169.ngrok-free.app/proxy/conversations/1/messages', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
