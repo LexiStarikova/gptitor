aead4e585d53a9bb1f1a9abc8f603ea279644d8e
Alexandra Starikova-Nasibullina <a.nasibullina@innopolis.university>
2024-06-25 10:13:26 +0300

    Update URL

--- src/conversationPanel.tsx
+++ src/conversationPanel.tsx
@@ -15,7 +15,7 @@ export const ConversationPanel: React.FC = () => {
         try {
             //Link to intermediate (local) API: should be changes frequently!
             //conversation_id = 1; To be modified
-            const response = await fetch('https://8662-188-130-155-169.ngrok-free.app/proxy/conversations/1/messages', {
+            const response = await fetch('https://202f-188-130-155-169.ngrok-free.app/proxy/conversations/1/messages', {
                 method: 'POST',
                 headers: {
                     'Content-Type': 'application/json',
