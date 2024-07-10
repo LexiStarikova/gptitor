652b78c5821324ee72c1eedcbeed3f2ef3fa2f3c
AnasxHamrouni <anaskhamaieshamrouni@gmail.com>
2024-07-10 22:35:45 +0300

    Fixed sendButton

--- React/src/conversationPanel.tsx
+++ React/src/conversationPanel.tsx
@@ -80,6 +80,10 @@ export const ConversationPanel: React.FC<ConversationPanelProps> = ({ isOpenS, c
         setRequests(prevRequests => [...prevRequests, newRequest]);
 
         setLoading(true);
+        if (isOpenS)
+            close();
+        if (isOpenD)
+            closeD();
 
         if (inputRef.current) {
             inputRef.current.style.height = 'auto';
