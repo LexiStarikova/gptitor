347f7e328f966910426654a19ec50fcfbedb0add
Alexandra Starikova-Nasibullina <a.nasibullina@innopolis.university>
2024-07-12 10:17:08 +0300

    Fix tasks issue

--- React/src/models/task.tsx
+++ React/src/models/task.tsx
@@ -1,13 +1,13 @@
 export class Task {
     task_id: number;
     task_name: string;
-    task_category: string;
-    task_description: string;
+    category: string;
+    description: string;
 
     constructor(task_id: number, task_name: string, task_category: string, task_description: string) {
         this.task_id = task_id;
         this.task_name = task_name;
-        this.task_category = task_category;
-        this.task_description = task_description;
+        this.category = task_category;
+        this.description = task_description;
     }
 }
