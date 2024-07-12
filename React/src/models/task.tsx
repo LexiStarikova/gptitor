c7c662c670cf3542ae14f26bda8718280b6bbfce
Alexandra Starikova-Nasibullina <a.nasibullina@innopolis.university>
2024-07-12 10:19:18 +0300

    Change task constructor variables to category and description

--- React/src/models/task.tsx
+++ React/src/models/task.tsx
@@ -4,10 +4,10 @@ export class Task {
     category: string;
     description: string;
 
-    constructor(task_id: number, task_name: string, task_category: string, task_description: string) {
+    constructor(task_id: number, task_name: string, category: string, description: string) {
         this.task_id = task_id;
         this.task_name = task_name;
-        this.category = task_category;
-        this.description = task_description;
+        this.category = category;
+        this.description = description;
     }
 }
